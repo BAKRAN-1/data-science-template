5257b83f65a6975d8b9baa139a19876774938e25
BAKRAN-1 <3bdullahbakran@gmail.com>
2024-11-21 23:14:52 +0800

    Tempral abstraction not complate

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
-
+from FrequencyAbstraction import FourierTransformation
 
 # --------------------------------------------------------------
 # Load data
@@ -109,4 +109,69 @@ df_squared["gyr_r"] = np.square(gyr_r)
 
 subset = df_squared[df_squared["set"] == 14]
 
-subset[["acc_r", "gyr_r"]].plot()
+subset[["acc_r", "gyr_r"]].plot(subplots=True)
+
+# --------------------------------------------------------------
+# Tempral abstraction
+# --------------------------------------------------------------
+
+df_temporal = df_squared.copy()
+NumAbs = NumericalAbstraction()
+
+perdictor_columns = perdictor_columns + ["acc_r", "gyr_r"]
+
+ws = int(1000 / 200)
+
+for col in perdictor_columns:
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "mean")
+    df_temporal = NumAbs.abstract_numerical(df_temporal, [col], ws, "std")
+
+
+df_temporal_list = []
+for s in df_temporal["set"].unique():
+    subset = df_temporal[df_temporal["set"] == s].copy()
+    for col in perdictor_columns:
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "mean")
+        subset = NumAbs.abstract_numerical(subset, [col], ws, "std")
+    df_temporal_list.append(subset)
+
+df_temporal = pd.concat(df_temporal_list)
+
+subset[["acc_y", "acc_y_temp_mean_ws_5", "acc_y_temp_std_ws_5"]].plot()
+subset[["gyr_y", "gyr_y_temp_mean_ws_5", "gyr_y_temp_std_ws_5"]].plot()
+
+
+# --------------------------------------------------------------
+# Tempral abstraction
+# --------------------------------------------------------------
+
+df_freq = df_temporal.copy().reset_index()
+FreqAbs = FourierTransformation()
+
+fs = int(1000 / 200)
+ws = int(2800 / 200)
+
+df_freq = FreqAbs.abstract_frequency(df_freq, ["acc_y"], ws, fs)
+
+df_freq.columns
+# visualize results
+subset = df_freq[df_freq["set"] == 15]
+subset[["acc_y"]].plot()
+subset[
+    [
+        "acc_y_max_freq",
+        "acc_y_freq_weighted",
+        "acc_y_pse",
+        "acc_y_freq_1.429_Hz_ws_14",
+        "acc_y_freq_2.5_Hz_ws_14",
+    ]
+].plot()
+
+df_freq_list = []
+for s in df_freq["set"].unique():
+    print(f"Apply Fourier transformation to set {s}")
+    subset = df_freq[df_freq["set"] == s].reset_index(drop=True).copy()
+    subset = FreqAbs.abstract_frequency(subset, perdictor_columns, ws, fs)
+    df_freq_list.append(subset)
+
+pd.concat(df_freq_list)
